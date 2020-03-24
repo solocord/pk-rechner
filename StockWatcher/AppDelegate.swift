2b68780018339584fb94ed0ce7a2176d197cbd15
solocord <122412912+solocord@users.noreply.github.com>
2020-03-24 05:38:16 +0000

    [jgitflow-plugin]updating develop poms to master versions to avoid merge conflicts

--- StockWatcher/AppDelegate.swift
+++ StockWatcher/AppDelegate.swift
@@ -14,4 +14,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunch
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override p
