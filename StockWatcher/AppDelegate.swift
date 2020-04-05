a80ea54078ee96e682103c88f66e5e5d04702863
solocord <122412912+solocord@users.noreply.github.com>
2020-04-05 18:14:46 +0000

    Fix defaults

--- StockWatcher/AppDelegate.swift
+++ StockWatcher/AppDelegate.swift
@@ -25,4 +25,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared res
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information t
