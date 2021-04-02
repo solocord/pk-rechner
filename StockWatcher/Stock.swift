687d5353a569b65fe88a1b1d333b4b664651f430
solocord <122412912+solocord@users.noreply.github.com>
2021-04-02 10:31:40 +0000

    station updates

--- StockWatcher/Stock.swift
+++ StockWatcher/Stock.swift
@@ -29,3 +29,58 @@ class Stock: NSObject, NSCoding {
     
     // MARK: - Types
     struct PropertyKey {
+        static let name = "name"
+        static let ticker = "ticker"
+    }
+    
+    // MARK: - Initialization
+    init?(name: String, ticker: String) {
+        
+        // Name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // Ticker must not be empty
+        guard !ticker.isEmpty else {
+            return nil
+        }
+        
+        // Initialize stored properties
+        self.name = name
+        self.ticker = ticker
+        //self.sma200 = sma200
+        //self.sma100 = sma100
+        //self.sma50 = sma50
+        //self.exDividendDate = exDividendDate
+        //self.dateAdded = dateAdded
+    }
+    
+    // MARK: - NSCoding
+    func encode(with aCoder: NSCoder) {
+        
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(ticker, forKey: PropertyKey.ticker)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer
+        // should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String
+            else {
+                os_log("Unable to decode the name for a Stock object.", log: OSLog.default, type: .debug)
+                
+                return nil
+        }
+        
+        guard let ticker = aDecoder.decodeObject(forKey: PropertyKey.ticker) as? String
+            else {
+                os_log("Unable to decode a ticker for a Stock object.", log: OSLog.default, type: .debug)
+                
+                return nil
+        }
+        
+        // Must call designated initializer.
+        self.init(name: name, ticker: ticker)
+    }
+}
