c0ed16efb20f5334564c7e7d56b06b4f1754f465
solocord <122412912+solocord@users.noreply.github.com>
2021-04-02 20:41:35 +0000

    Tab order

--- StockWatcher/StockTableViewCell.swift
+++ StockWatcher/StockTableViewCell.swift
@@ -12,4 +12,8 @@ class StockTableViewCell: UITableViewCell {
 
     // MARK: - Properties
     @IBOutlet weak var tickerLabel: UILabel!
-    @IBOutlet weak var nameLabel: 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    
