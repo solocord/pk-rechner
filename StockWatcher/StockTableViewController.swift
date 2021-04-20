63412976bb2747b26e4a45758c4c034c79a5e757
solocord <122412912+solocord@users.noreply.github.com>
2021-04-20 12:27:50 +0000

    Minor fixups

---
+++ StockWatcher/StockTableViewController.swift
@@ -0,0 +1,13 @@
+
+//
+//  StockTableViewController.swift
+//  StockWatcher
+//
+//  Created by Philip Clapper on 2/7/17.
+//
+//
+
+import UIKit
+import os.log
+
+class StockTableViewController: UITableViewController {
