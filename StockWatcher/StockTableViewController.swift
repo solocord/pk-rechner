b516507ba6dfc6583d582d9a902c5233d49be453
solocord <122412912+solocord@users.noreply.github.com>
2021-04-20 17:20:25 +0000

    Removed dependency

--- StockWatcher/StockTableViewController.swift
+++ StockWatcher/StockTableViewController.swift
@@ -11,3 +11,103 @@ import UIKit
 import os.log
 
 class StockTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var stocks = [Stock]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Use the edit button item provided by the table view controller
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        // Load any saved stocks, otherwise load sample data
+        if let savedStocks = loadStocks() {
+            stocks += savedStocks
+        } else {
+            // Load the sample data
+            loadSampleStocks()
+        }
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return stocks.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Table view cells are reused and should be queued using a cell identifier
+        // This is using the controller for the StockTableViewCell
+        let cellIdentifier = "StockTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? StockTableViewCell else {
+            fatalError("The dequeued cell is not an instance of StockTableViewCell.")
+        }
+        
+        // Fetches the appropriate stock for the data source layout
+        let stock = stocks[indexPath.row]
+
+        // Configure the cell...
+        cell.nameLabel.text = stock.name
+        cell.tickerLabel.text = stock.ticker
+        
+        return cell
+    }
+
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            stocks.remove(at: indexPath.row)
+            saveStocks()
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
