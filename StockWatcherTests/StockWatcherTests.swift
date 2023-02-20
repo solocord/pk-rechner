2d613a189a5d5830cde571e6c75cba8b64e9c375
solocord <122412912+solocord@users.noreply.github.com>
2023-02-20 21:11:49 +0000

    bump

--- StockWatcherTests/StockWatcherTests.swift
+++ StockWatcherTests/StockWatcherTests.swift
@@ -33,4 +33,9 @@ class StockWatcherTests: XCTestCase {
 //    }
 //
 //    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test me
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        super.tearDown()
+//    }
+    
+//    func testExample() {
+        
