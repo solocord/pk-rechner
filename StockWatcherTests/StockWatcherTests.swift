87ff403fe5b0a7c4fbb884b12e732f1a92265d6c
solocord <122412912+solocord@users.noreply.github.com>
2023-03-16 05:26:28 +0000

    update .gitignore file

--- StockWatcherTests/StockWatcherTests.swift
+++ StockWatcherTests/StockWatcherTests.swift
@@ -44,4 +44,6 @@ class StockWatcherTests: XCTestCase {
     
 //    func testPerformanceExample() {
         // This is an example of a performance test case.
-//        
+//        self.measure {
+            // Put the code you want to measure the time of here.
+/
