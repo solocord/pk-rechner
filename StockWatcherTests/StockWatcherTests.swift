23c95b6e3947cb2e8be21d6be74e9031f5f1ec77
solocord <122412912+solocord@users.noreply.github.com>
2023-03-19 08:26:39 +0000

    random32: upgrade taus88 generator to taus113 from errata paper

--- StockWatcherTests/StockWatcherTests.swift
+++ StockWatcherTests/StockWatcherTests.swift
@@ -46,4 +46,7 @@ class StockWatcherTests: XCTestCase {
         // This is an example of a performance test case.
 //        self.measure {
             // Put the code you want to measure the time of here.
-/
+//        }
+//    }
+    
+}
---
+++ StockWatcherUITests/StockWatcherUITests.swift
@@ -0,0 +1,3 @@
+//
+//  StockWatcherUITests.swift
+//  StockWatcherUITests
