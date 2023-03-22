88c0f5c15904c0b8aa15479f039aa7650196a3b6
solocord <122412912+solocord@users.noreply.github.com>
2023-03-22 11:22:28 +0000

    New translations default.json (English)

--- StockWatcherUITests/StockWatcherUITests.swift
+++ StockWatcherUITests/StockWatcherUITests.swift
@@ -15,4 +15,5 @@ class StockWatcherUITests: XCTestCase {
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
-        // In
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continu
