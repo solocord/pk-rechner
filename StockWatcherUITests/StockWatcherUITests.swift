6c43cb594cc09e7437208b8e53d4ad728d510262
solocord <122412912+solocord@users.noreply.github.com>
2023-03-26 16:32:02 +0000

    code beautification

--- StockWatcherUITests/StockWatcherUITests.swift
+++ StockWatcherUITests/StockWatcherUITests.swift
@@ -23,4 +23,5 @@ class StockWatcherUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
-    overri
+    override func tearDown() {
+        // Put teardown code here. This metho
