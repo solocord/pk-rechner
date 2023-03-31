c8a071f9fbc8a18e583f4581b791636ec34fc268
solocord <122412912+solocord@users.noreply.github.com>
2023-03-31 06:59:49 +0000

    Release v0.13.0

--- StockWatcherUITests/StockWatcherUITests.swift
+++ StockWatcherUITests/StockWatcherUITests.swift
@@ -25,4 +25,8 @@ class StockWatcherUITests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
- 
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use r
